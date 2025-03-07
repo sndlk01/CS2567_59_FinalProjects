db8b1732435b5ac8c58b67748aceb7f42899e843
chakit-pragod <chakit.p@kkumail.com>
2025-03-07 23:11:43 +0700

    set time out when input data form api to database

--- app/Services/TDBMApiService.php
+++ app/Services/TDBMApiService.php
@@ -1,78 +1,105 @@
 <?php
+
 namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
 
-class TDBMApiService 
+class TDBMApiService
 {
     private $baseUrl = 'https://tdbm.computing.kku.ac.th/api/get_data';
+    private $timeout = 180;
 
-    public function getTeachers() {
+    private function fetchPaginatedData($tableName, $page = 1, $perPage = 1000)
+    {
+        return Http::timeout($this->timeout)
+            ->get("{$this->baseUrl}?table_name={$tableName}&page={$page}&per_page={$perPage}")
+            ->json();
+    }
+
+    public function getTeachers()
+    {
         return $this->fetchData('teachers');
     }
 
-    public function getCurriculums() {
+    public function getCurriculums()
+    {
         return $this->fetchData('curriculums');
     }
 
-    public function getSubjects() {
+    public function getSubjects()
+    {
         return $this->fetchData('subjects');
     }
 
-    public function getCourses() {
+    public function getCourses()
+    {
         return $this->fetchData('courses');
     }
 
-    public function getSemesters() {
+    public function getSemesters()
+    {
         return $this->fetchData('semesters');
     }
 
-    public function getClassTypes() {
+    public function getClassTypes()
+    {
         return $this->fetchData('classtypes');
     }
 
-    public function getCoursesTeachers() {
+    public function getCoursesTeachers()
+    {
         return $this->fetchData('coursesteachers');
     }
 
-    public function getCurriculumSubjects() {
+    public function getCurriculumSubjects()
+    {
         return $this->fetchData('curriculumsubjects');
     }
 
-    public function getFileUploads() {
+    public function getFileUploads()
+    {
         return $this->fetchData('fileuploads');
     }
 
-    public function getMajors() {
+    public function getMajors()
+    {
         return $this->fetchData('majors');
     }
 
-    public function getProfiles() {
+    public function getProfiles()
+    {
         return $this->fetchData('profiles');
     }
 
-    public function getSchedules() {
+    public function getSchedules()
+    {
         return $this->fetchData('schedules');
     }
 
-    public function getStudentClasses() {
+    public function getStudentClasses()
+    {
         return $this->fetchData('studentclasses');
     }
 
-    public function getWageWeights() {
+    public function getWageWeights()
+    {
         return $this->fetchData('wageweights');
     }
 
-    public function getTeachings() {
+    public function getTeachings()
+    {
         return $this->fetchData('teachings');
     }
 
-    public function getExtraTeachings() {
+    public function getExtraTeachings()
+    {
         return $this->fetchData('extra_teachings');
     }
 
-    private function fetchData($tableName) {
-        return Http::get("{$this->baseUrl}?table_name={$tableName}")->json();
+    private function fetchData($tableName)
+    {
+        return Http::timeout($this->timeout)
+            ->get("{$this->baseUrl}?table_name={$tableName}")
+            ->json();
     }
-    
 }
